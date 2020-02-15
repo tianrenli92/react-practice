be78420bd527cfb5738f4e6112d54f42fd055914
Tianren Li <tianrenli92@gmail.com>
2020-02-14 20:22:24 -0500

    week 1 react basics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Course {
+    constructor(college, department, number, section, professor) {
+        this.college = college;
+        this.department = department;
+        this.number = number;
+        this.section = section;
+        this.professor = professor;
+        this.key = `${college} ${department} ${number} ${section}`;
+    }
+}
+
+class App extends Component {
+    render() {
+        const columns = [
+            'College',
+            'Department',
+            'Number',
+            'Section',
+            'Professor',
+        ];
+        const courses = [
+            new Course('CAS', 'CS', '112', 'B1', 'Sullivan'),
+            new Course('CAS', 'CS', '332', 'A1', 'Bun'),
+            new Course('CAS', 'CS', '350', 'A1', 'Sarkar'),
+        ];
+
+        return (
+            <div className="App">
+                <table>
+                    <thead>
+                    <tr>
+                        {columns.map(column =>
+                            <th key={column}>{column}</th>)}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        courses.map(course =>
+                            <tr key={course.key}>
+                                <td>{course.college}</td>
+                                <td>{course.department}</td>
+                                <td>{course.number}</td>
+                                <td>{course.section}</td>
+                                <td>{course.professor}</td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 export default App;
