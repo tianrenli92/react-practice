524ef9bc23493a1e615483fc0e0c763ea9f1451e
Tianren Li <tianrenli92@gmail.com>
2020-04-13 23:15:28 -0400

    Week 9

--- src/App.js
+++ src/App.js
@@ -15,16 +15,28 @@ class Course {
 class App extends Component {
     constructor(props) {
         super(props);
+
         const columns = [
             'College',
             'Department',
             'Number',
             'Section',
             'Professor',
+            '',
         ];
+
+        const searchTerms =
+            {
+                college: '',
+                department: '',
+                number: '',
+                section: '',
+                professor: '',
+            };
 
         this.state = {
             columns,
+            searchTerms,
             courses: [],
             myCourses: [],
         };
@@ -32,6 +44,9 @@ class App extends Component {
 
     getCourses = () => {
         const courses = [
+            new Course('CAS', 'CS', '113', 'B1', 'Sullivan'),
+            new Course('CAS', 'MA', '112', 'B1', 'Sullivan'),
+            new Course('ENG', 'CS', '112', 'B1', 'Sullivan'),
             new Course('CAS', 'CS', '112', 'B1', 'Sullivan'),
             new Course('CAS', 'CS', '332', 'A1', 'Bun'),
             new Course('CAS', 'CS', '350', 'A1', 'Sarkar'),
@@ -40,20 +55,68 @@ class App extends Component {
     };
 
 
-    addCourse(course) {
+    addCourse = (course) => {
         const {myCourses} = this.state;
-        myCourses.push(course);
+        if (myCourses.filter(myCourse => myCourse.key === course.key).length === 0) {
+            myCourses.push(course);
+        }
         this.setState({myCourses});
-    }
+    };
 
-    deleteCourse(course) {
+    deleteCourse = (course) => {
         let {myCourses} = this.state;
         myCourses = myCourses.filter(myCourse => myCourse.key !== course.key);
         this.setState({myCourses});
-    }
+    };
+
+    onCollegeSearchChanged = (event) => {
+        const college = event.target.value;
+        const {searchTerms} = this.state;
+        searchTerms.college = college;
+        this.setState({searchTerms});
+    };
+
+    onDepartmentSearchChanged = (event) => {
+        const department = event.target.value;
+        const {searchTerms} = this.state;
+        searchTerms.department = department;
+        this.setState({searchTerms});
+    };
+
+    onNumberSearchChanged = (event) => {
+        const number = event.target.value;
+        const {searchTerms} = this.state;
+        searchTerms.number = number;
+        this.setState({searchTerms});
+    };
+
+    onSectionSearchChanged = (event) => {
+        const section = event.target.value;
+        const {searchTerms} = this.state;
+        searchTerms.section = section;
+        this.setState({searchTerms});
+    };
+
+    onProfessorSearchChanged = (event) => {
+        const professor = event.target.value;
+        const {searchTerms} = this.state;
+        searchTerms.professor = professor;
+        this.setState({searchTerms});
+    };
+
+    courseFilter = (course, searchTerms) => {
+        const {college, department, number, section, professor} = searchTerms;
+        return (
+            (college === '' || course.college.startsWith(college)) &&
+            (department === '' || course.department.startsWith(department)) &&
+            (number === '' || course.number.startsWith(number)) &&
+            (section === '' || course.section.startsWith(section)) &&
+            (professor === '' || course.professor.startsWith(professor))
+        );
+    };
 
     render() {
-        const {columns, courses, myCourses} = this.state;
+        const {columns, searchTerms, courses, myCourses} = this.state;
         return (
             <div className="App">
                 <button onClick={this.getCourses}>Get courses</button>
@@ -64,21 +127,30 @@ class App extends Component {
                         {columns.map(column =>
                             <th key={column}>{column}</th>)}
                     </tr>
+                    <tr>
+                        <th><input onChange={this.onCollegeSearchChanged}/></th>
+                        <th><input onChange={this.onDepartmentSearchChanged}/></th>
+                        <th><input onChange={this.onNumberSearchChanged}/></th>
+                        <th><input onChange={this.onSectionSearchChanged}/></th>
+                        <th><input onChange={this.onProfessorSearchChanged}/></th>
+                    </tr>
                     </thead>
                     <tbody>
                     {
-                        courses.map(course =>
-                            <tr key={course.key}>
-                                <td>{course.college}</td>
-                                <td>{course.department}</td>
-                                <td>{course.number}</td>
-                                <td>{course.section}</td>
-                                <td>{course.professor}</td>
-                                <td>
-                                    <button onClick={() => this.addCourse(course)}>Add to my courses</button>
-                                </td>
-                            </tr>
-                        )
+                        courses
+                            .filter(course => this.courseFilter(course, searchTerms))
+                            .map(course =>
+                                <tr key={course.key}>
+                                    <td>{course.college}</td>
+                                    <td>{course.department}</td>
+                                    <td>{course.number}</td>
+                                    <td>{course.section}</td>
+                                    <td>{course.professor}</td>
+                                    <td>
+                                        <button onClick={() => this.addCourse(course)}>Add to my courses</button>
+                                    </td>
+                                </tr>
+                            )
                     }
                     </tbody>
                 </table>
